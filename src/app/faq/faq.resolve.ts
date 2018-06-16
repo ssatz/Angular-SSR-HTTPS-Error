41be4d73eb88dabdfeb16956685a8450b6d0260d
satz <sathish.thi@gmail.om>
2018-06-16 13:13:35 +0530

    chore : removed transfer state

--- src/app/faq/faq.resolve.ts
+++ src/app/faq/faq.resolve.ts
@@ -2,9 +2,9 @@
 import {of as observableOf, Observable} from 'rxjs';
 
 import {tap} from 'rxjs/operators';
-import {Injectable} from "@angular/core";
-import {Resolve, ActivatedRouteSnapshot} from "@angular/router";
-import {FAQService} from "../shared/service/faq.service";
+import {Injectable} from '@angular/core';
+import {Resolve, ActivatedRouteSnapshot} from '@angular/router';
+import {FAQService} from '../shared/service/faq.service';
 import {makeStateKey, TransferState} from '@angular/platform-browser';
 const FAQ_KEY = makeStateKey<any>('faq.result');
 @Injectable()
@@ -15,14 +15,13 @@ export class FAQResolver implements Resolve<any> {
     }
 
     resolve(_route: ActivatedRouteSnapshot): Observable<any> {
-        const found = this.transferState.hasKey(FAQ_KEY);
+       /* const found = this.transferState.hasKey(FAQ_KEY);
         if (found) {
             const res = observableOf(this.transferState.get<any>(FAQ_KEY, null));
             this.transferState.remove(FAQ_KEY);
             return res;
         }
-        this.transferState.onSerialize(FAQ_KEY, () => this.result);
-        return this._faqService.getFAQs().pipe(
-            tap(result => this.result = result));
+        this.transferState.onSerialize(FAQ_KEY, () => this.result);*/
+        return this._faqService.getFAQs();
     }
 }
